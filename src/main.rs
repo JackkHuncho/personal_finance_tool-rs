938f518ef278662198abe1ed2a0b9180ae115d65
Kendrix Henderson <kendrixhenderson@gmail.com>
2025-07-28 13:48:49 -0600

    cleaned main

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,15 @@ mod models;
 mod storage;
 mod utils;
 
-
 use clap::Parser;
 use crate::cli::FinCli;
-use crate::cli::commands::Commands;
-use crate::storage::file_handler;
-use crate::models::transaction;
 use crate::utils::validation;
-use prettytable;
 
 fn main() {
     // on init check if there is a data file, if not create one.
     validation::data_file_exists();
 
     let cli = FinCli::parse();
+    cli.command.process();
 
-    let mut transactions = file_handler::load_raw().unwrap();
-
-    match cli.command {
-        Commands::Add { date, amount, category, note } => {
-            transactions.push(transaction::Transaction::new(transactions.len() as u32 + 1, &date, &amount, &category, note).unwrap());
-            file_handler::save_raw(&transactions);
-        },
-        Commands::List {} => {
-           transaction::print_transactions(&transactions);
-        },
-    }
 }
